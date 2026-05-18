9bee2ffd5d0f4fff4da8132530eb79dcbf4aaa7b
nisrina salsa <nisrinahikmawan@gmail.com>
2026-05-18 11:17:44 +0700

    create function SecurityFilterChain in SecurityConfig

--- src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
+++ src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
@@ -5,11 +5,13 @@ import com.deploy.pertemuan11.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +33,28 @@ public class SecurityConfig {
                     .build();
 
         };
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers(
+                                "/register",
+                                "/login",
+                                "/css/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login-> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout(logout-> logout
+                        .logoutSuccessUrl("/login")
+                );
+        return http.build();
     }
 }
