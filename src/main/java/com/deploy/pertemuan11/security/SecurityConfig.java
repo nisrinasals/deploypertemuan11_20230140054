11e463d1d323428eb7c2e1029a4d60be99a209d4
nisrina salsa <nisrinahikmawan@gmail.com>
2026-05-18 11:07:19 +0700

    create function PasswordEncoder & UserDetailService in SecurityConfig

---
+++ src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.deploy.pertemuan11.security;
+
+import com.deploy.pertemuan11.model.User;
+import com.deploy.pertemuan11.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User tidak ditemukan"));
+
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(user.getUsername())
+                    .password(user.getPassword())
+                    .authorities("USER")
+                    .build();
+
+        };
+    }
+}
